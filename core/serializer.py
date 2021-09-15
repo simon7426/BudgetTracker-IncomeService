25c3106faf66d39015058e997408d8f9813678f4
Simon Islam <simon.islam@brilliant.com.bd>
2021-09-15 14:21:42 +0600

    Modified Serializers

--- core/serializer.py
+++ core/serializer.py
@@ -6,9 +6,9 @@ from .models import IncomeCategory, Income
 class IncomeCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = IncomeCategory
-        fields = ['id','income_category_name','income_category_owner','active_status','created_on','updated_on']
+        fields = '__all__'
     
 class IncomeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Income
-        fields = ['id','user_id','income_category_id','account_id','description','amount','created_on','updated_on']
+        fields = '__all__'
